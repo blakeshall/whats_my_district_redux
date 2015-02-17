533cdb056b3c7333ab373234effd7429111db3de
Blake Hall <blake@blakeshall.com>
2015-02-17 13:36:02 -0500

    Moving stuff around

---
+++ assets/js/lexington.js
@@ -0,0 +1,143 @@
+var map;
+var geocoder;
+var councilLayer;
+var elemLayer;
+var middleLayer;
+var highLayer;
+var magistrateLayer;
+var houseLayer;
+var senateLayer;
+var schoolBoardLayer;
+var votingLayer;
+var neighborhoodAssocLayer;
+var postOfficeLayer;
+var fireStationLayer;
+var libraryLayer;
+var hospitalLayer;
+var countyLayer;
+var postMarker;
+var fireMarker;
+var libraryMarker;
+var hospitalMarker;
+var lookupMarker;
+
+var initVariables = function(){
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYmxha2VzaGFsbCIsImEiOiJRSkN3Y3prIn0.MfDnpigJE6CVbEsV0xwLfA';
+    map;
+    geocoder = L.mapbox.geocoder('mapbox.places');
+    councilLayer = L.mapbox.featureLayer();
+    elemLayer = L.mapbox.featureLayer();
+    middleLayer = L.mapbox.featureLayer();
+    highLayer = L.mapbox.featureLayer();
+    magistrateLayer = L.mapbox.featureLayer();
+    houseLayer = L.mapbox.featureLayer();
+    senateLayer = L.mapbox.featureLayer();
+    schoolBoardLayer = L.mapbox.featureLayer();
+    votingLayer = L.mapbox.featureLayer();
+    neighborhoodAssocLayer = L.mapbox.featureLayer();
+    postOfficeLayer = L.mapbox.featureLayer();
+    fireStationLayer = L.mapbox.featureLayer();
+    libraryLayer = L.mapbox.featureLayer();
+    hospitalLayer = L.mapbox.featureLayer();
+    countyLayer = L.mapbox.featureLayer();
+    postMarker = L.mapbox.featureLayer();
+    fireMarker = L.mapbox.featureLayer();
+    libraryMarker = L.mapbox.featureLayer();
+    hospitalMarker = L.mapbox.featureLayer();
+    lookupMarker = L.marker([0, 0], {
+    	"marker-color": "#3bb20"
+    });
+}
+
+var loadData = function(){
+    councilLayer.loadURL('assets/data/council.geojson');
+  //   .on('ready', function(data){
+  //   	councilLayer.eachLayer(function(layer) {
+		// 	layer.bindPopup('District: <strong>' + layer.feature.properties.DISTRICT + '</strong>', { closeButton: false});
+		// });
+  //   })
+    elemLayer.loadURL('assets/data/elem.geojson');
+    middleLayer.loadURL('assets/data/middle.geojson');
+    highLayer.loadURL('assets/data/high.geojson');
+    magistrateLayer.loadURL('assets/data/magistrate.geojson');
+    houseLayer.loadURL('assets/data/house.geojson');
+    senateLayer.loadURL('assets/data/senate.geojson');
+    schoolBoardLayer.loadURL('assets/data/school-board.geojson');
+    votingLayer.loadURL('assets/data/voting.geojson');
+    postOfficeLayer.loadURL('assets/data/post-office.geojson');
+    fireStationLayer.loadURL('assets/data/fire-station.geojson');
+    libraryLayer.loadURL('assets/data/library.geojson');
+    hospitalLayer.loadURL('assets/data/hospital.geojson');
+    neighborhoodAssocLayer.loadURL('assets/data/neighborhood-assoc.geojson');
+    countyLayer.loadURL('assets/data/county.geojson');
+    
+}
+
+var findDistricts = function(point) {
+    var postOffice = findNearest(postOfficeLayer, point)
+    var fireStation = findNearest(fireStationLayer, point);
+    var library = findNearest(libraryLayer, point);
+    var hospital = findNearest(hospitalLayer, point);
+    setMarker(postOffice, postMarker, '#63b6e5', 'post', 'small', postOffice.properties.NAME, postOffice.properties.ADDRESS, map);
+    setMarker(fireStation, fireMarker,  '#FF0000', 'fire-station', 'small', fireStation.properties.MORE, fireStation.properties.ADDRESS, map);
+    setMarker(library, libraryMarker,  '#57FF65', 'library', 'small', library.properties.NAME, library.properties.ADDRESS, map);
+    setMarker(hospital, hospitalMarker,  '#FFFFFF', 'hospital', 'small', hospital.properties.NAME, hospital.properties.ADDRESS, map);
+    setTemplate(findDistrict(point, councilLayer), '#council-template', '#council-results');
+    setTemplate(findDistrict(point, elemLayer), '#elem-template', '#elem-results');
+    setTemplate(findDistrict(point, middleLayer), '#middle-template', '#middle-results');
+    setTemplate(findDistrict(point, highLayer), '#high-template', '#high-results');
+    setTemplate(findDistrict(point, houseLayer), '#house-template', '#house-results');
+    setTemplate(findDistrict(point, senateLayer), '#senate-template', '#senate-results');
+    setTemplate(findDistrict(point, schoolBoardLayer), '#school-board-template', '#school-board-results');
+    setTemplate(findDistrict(point, votingLayer), '#voting-template', '#voting-results');
+    setTemplate(findDistrict(point, magistrateLayer), '#magistrate-template', '#magistrate-results');
+    // setTemplate(findDistrict(point, neighborhoodAssocLayer), '#neighborhood-assoc-template', '#neighborhood-assoc-results');
+    lookupMarker.setLatLng([point.geometry.coordinates[1], point.geometry.coordinates[0]]);
+    lookupMarker.setOpacity(1)
+
+};
+
+var addressLookup = function(query) {
+    geocoder.query(query, function(err, data){
+        if (data.latlng) {
+            map.setView([data.latlng[0], data.latlng[1]], 13);
+            var point = getPoint(data.latlng[0], data.latlng[1]);
+            findDistricts(point);
+        }
+    });
+};
+
+$(document).ready(function(){
+    initVariables();
+
+    map = L.mapbox.map('map', 'mapbox.light')
+        .setView([38.05, -84.5], 12);
+
+    lookupMarker.setOpacity(0)
+    lookupMarker.addTo(map);
+    loadData();
+    countyLayer.addTo(map);
+    
+    countyLayer.on('click', function(e) {
+        var point = getPoint(e.latlng.lat, e.latlng.lng);
+        findDistricts(point);
+        
+    });
+    $('#search').on('click', function(e){
+        e.preventDefault();
+        addressLookup($('#search-address').val() + ' Lexington , KY');
+    });
+
+    postMarker.on('mouseover', function(e){
+        e.layer.openPopup();
+    });
+    fireMarker.on('mouseover', function(e){
+        e.layer.openPopup();
+    });
+    libraryMarker.on('mouseover', function(e){
+        e.layer.openPopup();
+    });
+    hospitalMarker.on('mouseover', function(e){
+        e.layer.openPopup();
+    });
+})
--- assets/js/whats-my-district.js
+++ assets/js/whats-my-district.js
@@ -1,53 +1,5 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoiYmxha2VzaGFsbCIsImEiOiJRSkN3Y3prIn0.MfDnpigJE6CVbEsV0xwLfA';
-var map;
-var geocoder = L.mapbox.geocoder('mapbox.places');
-var councilLayer = L.mapbox.featureLayer();
-var elemLayer = L.mapbox.featureLayer();
-var middleLayer = L.mapbox.featureLayer();
-var highLayer = L.mapbox.featureLayer();
-var magistrateLayer = L.mapbox.featureLayer();
-var houseLayer = L.mapbox.featureLayer();
-var senateLayer = L.mapbox.featureLayer();
-var schoolBoardLayer = L.mapbox.featureLayer();
-var votingLayer = L.mapbox.featureLayer();
-var neighborhoodAssocLayer = L.mapbox.featureLayer();
-var postOfficeLayer = L.mapbox.featureLayer();
-var fireStationLayer = L.mapbox.featureLayer();
-var libraryLayer = L.mapbox.featureLayer();
-var hospitalLayer = L.mapbox.featureLayer();
-var countyLayer = L.mapbox.featureLayer();
-var postMarker = L.mapbox.featureLayer();
-var fireMarker = L.mapbox.featureLayer();
-var libraryMarker = L.mapbox.featureLayer();
-var hospitalMarker = L.mapbox.featureLayer();
-var lookupMarker = L.marker([0, 0], {
-	"marker-color": "#3bb20"
-});
 
-var loadData = function(){
-    councilLayer.loadURL('assets/data/council.geojson');
-  //   .on('ready', function(data){
-  //   	councilLayer.eachLayer(function(layer) {
-		// 	layer.bindPopup('District: <strong>' + layer.feature.properties.DISTRICT + '</strong>', { closeButton: false});
-		// });
-  //   })
-    elemLayer.loadURL('assets/data/elem.geojson');
-    middleLayer.loadURL('assets/data/middle.geojson');
-    highLayer.loadURL('assets/data/high.geojson');
-    magistrateLayer.loadURL('assets/data/magistrate.geojson');
-    houseLayer.loadURL('assets/data/house.geojson');
-    senateLayer.loadURL('assets/data/senate.geojson');
-    schoolBoardLayer.loadURL('assets/data/school-board.geojson');
-    votingLayer.loadURL('assets/data/voting.geojson');
-    postOfficeLayer.loadURL('assets/data/post-office.geojson');
-    fireStationLayer.loadURL('assets/data/fire-station.geojson');
-    libraryLayer.loadURL('assets/data/library.geojson');
-    hospitalLayer.loadURL('assets/data/hospital.geojson');
-    neighborhoodAssocLayer.loadURL('assets/data/neighborhood-assoc.geojson');
-    countyLayer.loadURL('assets/data/county.geojson');
-    
-}
-
+// Returns the properties of the district polygon the point is in
 var findDistrict = function(point, districtLayer){
 	var district;
 	districtLayer.eachLayer(function(layer){
@@ -58,6 +10,7 @@ var findDistrict = function(point, districtLayer){
 	return district.feature.properties;
 }
 
+// Returns a geojson point of the nearest feature of a featureLayer
 var findNearest = function (featureLayer, point) {
 	var nearest;
 	var geojson = featureLayer.getGeoJSON();
@@ -65,15 +18,19 @@ var findNearest = function (featureLayer, point) {
 	return nearest;
 }
 
-var setMarker = function(point, markerLayer, color, icon, size){
+// Sets the marker properties and adds it to the map
+var setMarker = function(point, markerLayer, color, icon, size, title, description, map){
 	point.properties['marker-color'] = color;
 	point.properties['marker-symbol'] = icon;
 	point.properties['marker-size'] = size;
+	point.properties['title'] = title;
+	point.properties['description'] = description;
 	map.removeLayer(markerLayer);
 	markerLayer.setGeoJSON(point);
 	markerLayer.addTo(map);
 }
 
+// Get geojson point from a lat and lng pair
 var getPoint = function(lat, lng){
 	var point = {
 		"type": "Feature",
@@ -86,67 +43,10 @@ var getPoint = function(lat, lng){
 	return point;
 }
 
+// Set the Handlebars template
 var setTemplate = function(props, sourceTemplate, output){
 	var source = $(sourceTemplate).html();
 	var template = Handlebars.compile(source);
 	var html = template(props);
 	$(output).html(html);
 }
-
-var addressLookup = function(query) {
-	geocoder.query(query, function(err, data){
-		if (data.latlng) {
-	        map.setView([data.latlng[0], data.latlng[1]], 13);
-	        var point = getPoint(data.latlng[0], data.latlng[1]);
-	        findDistricts(point);
-	    }
-	});
-};
-
-var findDistricts = function(point) {
-	var postOffice = findNearest(postOfficeLayer, point)
-	var fireStation = findNearest(fireStationLayer, point);
-	var library = findNearest(libraryLayer, point);
-	var hospital = findNearest(hospitalLayer, point);
-	setMarker(postOffice, postMarker, '#63b6e5', 'post', 'small');
-	setMarker(fireStation, fireMarker,  '#FF0000', 'fire-station', 'small');
-	setMarker(library, libraryMarker,  '#57FF65', 'library', 'small');
-	setMarker(hospital, hospitalMarker,  '#FFFFFF', 'hospital', 'small');
-	setTemplate(findDistrict(point, councilLayer), '#council-template', '#council-results');
-	setTemplate(findDistrict(point, elemLayer), '#elem-template', '#elem-results');
-	setTemplate(findDistrict(point, middleLayer), '#middle-template', '#middle-results');
-	setTemplate(findDistrict(point, highLayer), '#high-template', '#high-results');
-	setTemplate(findDistrict(point, houseLayer), '#house-template', '#house-results');
-	setTemplate(findDistrict(point, senateLayer), '#senate-template', '#senate-results');
-	setTemplate(findDistrict(point, schoolBoardLayer), '#school-board-template', '#school-board-results');
-	setTemplate(findDistrict(point, votingLayer), '#voting-template', '#voting-results');
-	setTemplate(findDistrict(point, magistrateLayer), '#magistrate-template', '#magistrate-results');
-	// setTemplate(findDistrict(point, neighborhoodAssocLayer), '#neighborhood-assoc-template', '#neighborhood-assoc-results');
-	lookupMarker.setLatLng([point.geometry.coordinates[1], point.geometry.coordinates[0]]);
-	lookupMarker.setOpacity(1)
-
-};
-
-councilLayer.eachLayer(function(layer) {
-	layer.bindPopup('District: <strong>' + layer.feature.properties.DISTRICT + '</strong>', { closeButton: false});
-});
-
-$(document).ready(function(){
-	map = L.mapbox.map('map', 'mapbox.light')
-		.setView([38.05, -84.5], 12);
-
-	lookupMarker.setOpacity(0)
-	lookupMarker.addTo(map);
-	loadData();
-	countyLayer.addTo(map);
-	
-	countyLayer.on('click', function(e) {
-		var point = getPoint(e.latlng.lat, e.latlng.lng);
-		findDistricts(point);
-		
-	});
-	$('#search').on('click', function(e){
-		e.preventDefault();
-		addressLookup($('#search-address').val() + ' Lexington , KY');
-	});
-})
