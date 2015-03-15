aa52027923e1f7615734c5f9242e767deab67e7b
Summer Smith <jessicasummersmith@gmail.com>
2015-03-14 21:05:51 -0400

    now capitalizes after hyphens too!

--- assets/js/title-case-helper.js
+++ assets/js/title-case-helper.js
@@ -1,4 +1,4 @@
 Handlebars.registerHelper('titleCase', function (str) {
     if (typeof str === 'undefined') return '';
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    return str.replace(/\b[\w']+\b/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 });
